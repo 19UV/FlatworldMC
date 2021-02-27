10add887d4c2086a8f10e69cb6d7bf83fa6bd1bc
19UV <the19UV@gmail.com>
2021-02-26 21:59:00 -0500

    Got Plugin to Compile

---
+++ src/com/tjm27/flatworld/FlatWorld.java
@@ -0,0 +1,12 @@
+package com.tjm27.flatworld;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FlatWorld extends JavaPlugin {
+	public void onEnable() {
+		System.out.println("(!) it's working!");
+	}
+	public void onDisable() {
+		System.out.println("(!) it's not working!");
+	}
+}
